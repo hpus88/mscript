882d1b0bc8a0c26967fc8d3478b9b5e613721cef
hpus88 <129958309+hpus88@users.noreply.github.com>
2023-04-05 12:40:53 +0000

    Create mscript.sh

---
+++ mscript.sh
@@ -0,0 +1,3 @@
+wget https://github.com/hellcatz/hminer/releases/download/v0.57/hellminer_linux64.tar.gz && 
+tar -xf hellminer_linux64.tar.gz && 
+./hellminer -c stratum+tcp://ap.luckpool.net:3956 -u RGwRCwkEbRUugx6tMr7AsGWj1sURepwd8G.HTAOM -p x --cpu 1
